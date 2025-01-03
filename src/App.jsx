07a3a0bb2d8f6cc255975467b73b410b5f10561c
Akachukwu <egbolucheakachukwu@gmail.com>
2025-01-03 21:12:52 +0100

    Updated Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import Footer from "./components/Footer"
 import LeadershipSection from "./components/LeadershipSection"
 import EventsSection from "./components/EventsSection"
 import StoriesSection from "./components/TestimonySection"
+import { Analytics } from '@vercel/analytics/next';
 
 
 const App = () => {
   return (
     <div className="bg-black text-white min-h-screen flex flex-col">
+      <Analytics />
       <Navbar />
       <main className="flex-grow">
         <Hero />
